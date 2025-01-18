bd347cd5eefc059dc03dbd8e542f8f2ca732eecb
Rohith Gowda M <rohithgowda150503@gmail.com>
2025-01-18 21:17:41 +0530

    blog1chapter3

--- src/routes/blogs/+page.svelte
+++ src/routes/blogs/+page.svelte
@@ -5,7 +5,7 @@
     import Blog2 from '$lib/BlogCover/Blog2.jpeg'
     import Blog3 from '$lib/BlogCover/Blog3.jpeg'
     import blog1chapter2 from './blogs/blog1/ch2/ch2.md'
-    // import blog1chapter3 from './blogs/blog1/ch3/ch3.md'
+    import blog1chapter3 from './blogs/blog1/ch3/ch3.md'
     // import blog1chapter4 from './blogs/blog1/ch4/ch4.md'
     // import blog1chapter5 from './blogs/blog1/ch5/ch5.md'
 
@@ -18,13 +18,13 @@
     chapters: [
       { id: 1, title: "Cruising into the unexplored (1940-1960)", markdown: blog1chapter1 },
       { id: 2, title: "Tryst with Moon(1960-1970)", markdown: blog1chapter2 },
-      // { id: 3, title: "Understanding our neighbours better (1970-90)", markdown: blog1chapter3 },
+      { id: 3, title: "Understanding our neighbours better (1970-90)", markdown: blog1chapter3 },
       // { id: 4, title: "A harbour to sail in the stellar winds (1990-2000)", markdown: blog1chapter4 },
       // { id: 5, title: "Current Affairs (2000-2024)", markdown: blog1chapter5 },
     ],
-    nextChapterDate: "12-01-2025",
+    nextChapterDate: "25-01-2025",
     image:Blog1,
-    lastUpdated:'05-1-2024',
+    lastUpdated:'18-1-2024',
   },
   {
     id: 2,
